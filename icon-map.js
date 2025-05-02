4bf164aa571fe53ab892c18b5025d78cba237fbb
Yuki Kawahara <yuki.kawahar@gmail.com>
2025-05-02 12:38:08 -0500

    Added icons and icon functionality

---
+++ icon-map.js
@@ -0,0 +1,45 @@
+const iconMap = {
+    "-":          { type: "text", value: "-" }, // fallback
+
+    // Font Awesome examples
+    "Large":      { type: "fa", value: "fa-expand" },
+    "Armored":    { type: "fa", value: "fa-shield-alt" },
+    "Tank":       { type: "fa", value: "fa-truck-monster" },
+    "Steel":      { type: "fa", value: "fa-industry" },
+    "Fighter":    { type: "fa", value: "fa-fist-raised" },
+    "Mage":       { type: "fa", value: "fa-hat-wizard" },
+
+
+    // Image examples
+    "Psychic":    { type: "img", value: "icons/psychic.svg" },
+    "Shadowy":    { type: "img", value: "icons/shadowy.svg" },
+
+
+    // Types
+    "Normal":    { type: "img", value: "icons/types/Normal_icon_HOME3.png" },
+    "Fighting":    { type: "img", value: "icons/types/Fighting_icon_HOME3.png" },
+    "Flying":    { type: "img", value: "icons/types/Flying_icon_HOME3.png" },
+    "Poison":    { type: "img", value: "icons/types/Poison_icon_HOME3.png" },
+    "Ground":    { type: "img", value: "icons/types/Ground_icon_HOME3.png" },
+    "Rock":    { type: "img", value: "icons/types/Rock_icon_HOME3.png" },
+    "Bug":    { type: "img", value: "icons/types/Bug_icon_HOME3.png" },
+    "Ghost":    { type: "img", value: "icons/types/Ghost_icon_HOME3.png" },
+    "Steel":    { type: "img", value: "icons/types/Steel_icon_HOME3.png" },
+    "Fire":    { type: "img", value: "icons/types/Fire_icon_HOME3.png" },
+    "Water":    { type: "img", value: "icons/types/Water_icon_HOME3.png" },
+    "Grass":    { type: "img", value: "icons/types/Grass_icon_HOME3.png" },
+    "Electric":    { type: "img", value: "icons/types/Electric_icon_HOME3.png" },
+    "Psychic":    { type: "img", value: "icons/types/Psychic_icon_HOME3.png" },
+    "Ice":    { type: "img", value: "icons/types/Ice_icon_HOME3.png" },
+    "Dragon":    { type: "img", value: "icons/types/Dragon_icon_HOME3.png" },
+    "Dark":    { type: "img", value: "icons/types/Dark_icon_HOME3.png" },
+    "Fairy":    { type: "img", value: "icons/types/Fairy_icon_HOME3.png" },
+
+    //Categories
+    "Contact":    { type: "img", value: "icons/category/PhysicalIC_Masters.png" },
+    "Ranged":    { type: "img", value: "icons/category/SpecialIC_Masters.png" },
+    "Status":    { type: "img", value: "icons/category/StatusIC_Masters.png" }
+
+
+  };
+  
